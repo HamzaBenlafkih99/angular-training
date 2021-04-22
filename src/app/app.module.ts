53ac78acc3d08f1867b16ba9aa45e2300ea1d3fb
hamza benlafkih <hamzabenlafkih90@gmail.com>
2021-04-22 21:36:56 +0100

    navbar ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AddClientsComponent } from './components/add-clients/add-clients.component';
+import { EditClientsComponent } from './components/edit-clients/edit-clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistreComponent } from './components/registre/registre.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    ClientsComponent,
+    SidebarComponent,
+    AddClientsComponent,
+    EditClientsComponent,
+    ClientDetailsComponent,
+    LoginComponent,
+    RegistreComponent,
+    SettingsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule
